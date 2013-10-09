ebc325fe7a0af933f8a58e1d713426311e0f07a0
amey <gamoholic010@gmail.com>
2013-10-09 14:47:53 -0400

    inclass 13.10.09

--- in-class/2013-10-09/shared_todo_list/static/js/todo.js
+++ in-class/2013-10-09/shared_todo_list/static/js/todo.js
@@ -3,5 +3,15 @@
 
 // Insert code here to run when the DOM is ready
 $(document).ready( function() {
-
+	$(".delete-btn").click( {
+		
+		var URL = this.attr("item-id");
+		
+		window.location="/shared-todo-list/delete-item/"+"URL";
+		this.css("visibility","hidden");
+		this.remove();
+	
+	}	
+	)
+	
 });
