048028e236c6d0c3687254719d8be796d5edbccd
Charlie Garrod <charlie@cs.cmu.edu>
2013-10-30 07:53:31 -0400

    Webapps staff file distribution

---
+++ in-class/2013-10-30/src/edu/cmu/cs/webapps/reflection/Main.java
@@ -0,0 +1,11 @@
+package edu.cmu.cs.webapps.reflection;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Use reflection to access fields in the Person class 
+
+	}
+
+}
---
+++ in-class/2013-10-30/src/edu/cmu/cs/webapps/reflection/Person.java
@@ -0,0 +1,14 @@
+package edu.cmu.cs.webapps.reflection;
+
+public class Person {
+    public String name;  // This is public for demonstration purposes
+                         // only.  It is very poor design for the name
+                         // to be public
+    private int age;
+    
+    public Person(String name, int age) {
+    	   this.name = name;
+    	   this.age = age;
+    }
+    
+}
